d1a8c43e12655a3a4b19d4835f7d37d0379a2a8e
Ajdin <ajdinsinanovic100@gmail.com>
2022-02-09 18:58:25 +0100

    Advanced Vjezba 1.1

--- src/human/FirstExecutor.java
+++ src/human/FirstExecutor.java
@@ -1,14 +1,27 @@
 package human;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class FirstExecutor {
     public static void main(String[] args) {
         PersonReader personReader = new PersonReader();
         List<Person> personList = personReader.loadPerson();
-
+        Iterator<Person> personIterator = personList.iterator();
+        System.out.println("--------Iterator-------");
+        while (personIterator.hasNext()){
+            Person person = personIterator.next();
+            if (person.getName().equals("Sinan")){
+                personIterator.remove();
+            }
+        }
         for (Person person : personList){
             System.out.println(person);
         }
+        System.out.println("--------Spliterator-------");
+
+        personList.forEach(System.out::println);
+
     }
 }
